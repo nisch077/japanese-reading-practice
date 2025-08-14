ed3df150092d4922d0e24561352d67a421968f8f
Nischal Nagarkoti <nischal.nagarkoti46@gmail.com>
2025-08-14 16:47:58 +0545

    Prepared for score tracker feature, implemented keyboard and fixed CSS.

--- kanji_script.js
+++ kanji_script.js
@@ -1,6 +1,14 @@
 // A list of Kanji to use for the exercises is defined in kanji_data.js
 let currentExerciseIndex = 0;
 
+// A simple function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 // Get references to HTML elements from kanji.html
 const exerciseContainer = document.getElementById('exercise-container');
 const modalOverlay = document.getElementById('modal-overlay');
@@ -121,5 +129,78 @@ restartButton.addEventListener('click', function() {
     displayExercise();
 });
 
-// Start the first exercise when the page loads
-window.onload = displayExercise;
+// Function to create and render the virtual keyboard
+function createKeyboard() {
+    const keyboardContainer = document.getElementById('keyboard');
+    const inputField = document.getElementById('answer-input');
+
+    // Mapping for voiced (tenten) and semi-voiced (maru) consonants
+    const tentenMap = {
+        'か': 'が', 'き': 'ぎ', 'く': 'ぐ', 'け': 'げ', 'こ': 'ご',
+        'さ': 'ざ', 'し': 'じ', 'す': 'ず', 'せ': 'ぜ', 'そ': 'ぞ',
+        'た': 'だ', 'ち': 'ぢ', 'つ': 'づ', 'て': 'で', 'と': 'ど',
+        'は': 'ば', 'ひ': 'び', 'ふ': 'ぶ', 'へ': 'べ', 'ほ': 'ぼ'
+    };
+    const maruMap = {
+        'は': 'ぱ', 'ひ': 'ぴ', 'ふ': 'ぷ', 'へ': 'ぺ', 'ほ': 'ぽ'
+    };
+
+    const hiraganaKeys = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん', 'ゃ', 'ゅ', 'ょ', '、', '。'];
+
+    // Add all the basic hiragana keys
+    hiraganaKeys.forEach(char => {
+        const key = document.createElement('div');
+        key.className = 'key';
+        key.textContent = char;
+        key.addEventListener('click', () => {
+            inputField.value += char;
+            inputField.focus();
+        });
+        keyboardContainer.appendChild(key);
+    });
+
+    // Add special character keys
+    const specialKeys = ['゛', '゜', 'っ', 'Backspace'];
+    specialKeys.forEach(char => {
+        const key = document.createElement('div');
+        key.className = 'key';
+        key.textContent = char;
+
+        if (char === '゛') {
+            key.addEventListener('click', () => {
+                const lastChar = inputField.value.slice(-1);
+                if (tentenMap[lastChar]) {
+                    inputField.value = inputField.value.slice(0, -1) + tentenMap[lastChar];
+                }
+                inputField.focus();
+            });
+        } else if (char === '゜') {
+            key.addEventListener('click', () => {
+                const lastChar = inputField.value.slice(-1);
+                if (maruMap[lastChar]) {
+                    inputField.value = inputField.value.slice(0, -1) + maruMap[lastChar];
+                }
+                inputField.focus();
+            });
+        } else if (char === 'っ') {
+            key.addEventListener('click', () => {
+                inputField.value += 'っ';
+                inputField.focus();
+            });
+        } else if (char === 'Backspace') {
+            key.className += ' backspace';
+            key.addEventListener('click', () => {
+                inputField.value = inputField.value.slice(0, -1);
+                inputField.focus();
+            });
+        }
+        keyboardContainer.appendChild(key);
+    });
+}
+
+// Start the first exercise and create the keyboard when the page loads
+window.onload = function() {
+    shuffleArray(kanjiExercises);
+    displayExercise();
+    createKeyboard();
+};
